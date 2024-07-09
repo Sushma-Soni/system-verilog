c1948cfc4817a08d5480724105d47097dfe9cd1a
Sushma <88388730+Sushma-Soni@users.noreply.github.com>
2024-07-09 22:47:37 +0530

    Create enum_basic.sv

---
+++ Data_types/enum_basic.sv
@@ -0,0 +1,15 @@
+module enum_datatype;
+
+  //declaration
+  enum { red, green, blue, yellow, white, black } Colors;
+  
+  //display members of Colors
+  initial  begin
+    Colors= Colors.new;
+    for(int i=0;i<6;i++) begin
+      $display("Colors :: Value of  %0s \t is = %0d",Colors.name,Colors);
+      Colors = Colors.next;      
+    end
+    
+    
+  end
