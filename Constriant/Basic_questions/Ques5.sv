f9fb171c1a130d5d588e1b37d56987d4102efb89
Sushma <sushmasoni1351@gmail.com>
2024-07-23 22:25:40 +0530

    Question5
     write a constraint for 16 bit variable such that no consecutive ones should be generated .

---
+++ Constriant/Basic_questions/Ques5.sv
@@ -0,0 +1,24 @@
+// write a constraint for 16 bit variable such that no consecutive ones should be genrated
+
+class variable;
+  rand bit [15:0] value ;
+  constraint C1 { (value >0 );}
+  
+  function void post_randomize();
+    
+    for(int i=0;i<16;i++)begin
+    if(value[i]==0) value[i+1]=1;
+    else value[i+1]=0;
+    end 
+  endfunction
+endclass
+
+module tb;
+  variable num ;
+  initial begin
+    num =new();
+    repeat(10) begin
+      num.randomize();
+      $display("Value = %b",num.value); end
+  end
+endmodule
