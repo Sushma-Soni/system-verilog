45291d9083ad6da5ee4c7a56bb61d710724a96c9
Sushma <88388730+Sushma-Soni@users.noreply.github.com>
2024-08-28 14:04:59 +0530

    Update for_loop-ex2.sv

--- Control Flow/Loop Statements/for_loop-ex2.sv
+++ Control Flow/Loop Statements/for_loop-ex2.sv
@@ -17,8 +17,18 @@ module tb;
        temp =array[i];
      array[i] = array[n-1-i]; 
      array[n-1-i]= temp; 
-       $display("iteration i and array %0p",i,array);
+       $display("iteration %0d and array %0p",i,array);
      end
    $display("Reversed array %0p",array);
   end
 endmodule
+//---------------Output----------------
+// Aug 28 04:33 2024
+//          8
+// iteration 0 and array '{9, 3, 4, 5, 6, 7, 8, 2} 
+// iteration 1 and array '{9, 8, 4, 5, 6, 7, 3, 2} 
+// iteration 2 and array '{9, 8, 7, 5, 6, 4, 3, 2} 
+// iteration 3 and array '{9, 8, 7, 6, 5, 4, 3, 2} 
+// Reversed array '{9, 8, 7, 6, 5, 4, 3, 2} 
+//           V C S   S i m u l a t i o n   R e p o r t 
+// Time: 5 ns
